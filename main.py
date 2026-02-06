6eb31fbe6c2567023f26ef86f9ef9575f837ad62
sharma111008-a11y <sharma111008@gmail.com>
2026-02-06 17:36:49 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from kivy.app import App
+from kivy.uix.button import Button
+
+class MainApp(App):
+    def build(self):
+        return Button(text="🎰 VIP CASINO LIVE 🎰")
+
+if __name__ == "__main__":
+    MainApp().run()
+  
